ae2bc567bced5d2a743723f475e04a967f249b9c
M. Gage Morgan <gage@gages.blog>
2020-03-08 23:40:44 -0400

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -4,8 +4,8 @@ def my_collect(array)
         collection = []
     
         while i < array.length
-        collection << yield(array[i])
-        i += 1
+          collection << yield(array[i])
+          i += 1
         end
 
         collection
